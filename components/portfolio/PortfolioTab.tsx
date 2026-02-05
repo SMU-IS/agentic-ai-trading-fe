055b6916a76c7120d6e636ea8e36063583d6bf36
joshydavid <cxang.2022@scis.smu.edu.sg>
2026-02-05 14:45:09 +0800

    fix: increase status font size
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- components/portfolio/PortfolioTab.tsx
+++ components/portfolio/PortfolioTab.tsx
@@ -1,13 +1,13 @@
 "use client"
 
+import { getCompanyName } from "@/lib/tickerMap"
 import { StockWithHistory } from "@/lib/types"
 import { useEffect, useState } from "react"
+import MarketNews from "../news/MarketNews"
 import HoldingsTable from "./HoldingsTable"
 import PerformanceChart from "./PerformanceChart"
 import StockHistoryModal from "./StockHistoryModal"
 import SummaryCards from "./SummaryCards"
-import { getCompanyName } from "@/lib/tickerMap"
-import MarketNews from "../news/MarketNews"
 
 type AccountResponse = {
   cash: string
@@ -92,18 +92,18 @@ export default function PortfolioTab() {
         <div className="mb-6 flex items-right justify-end content-end">
           {loading ? (
             <div className="flex animate-pulse items-center gap-2">
-              <div className="h-2 w-2 rounded-full bg-gray-600" />
-              <div className="h-3 w-48 rounded bg-gray-600" />
+              <div className="h-3 w-3 rounded-full bg-gray-600" />
+              <div className="h-5 w-72 rounded bg-gray-600" />
             </div>
           ) : (
             <div className="flex items-center gap-2 justify-right">
               <span
-                className={`h-2 w-2 rounded-full ${
-                  tradingAccStatus ? "animate-pulse bg-teal-500" : "bg-red-500"
+                className={`h-3 w-3 rounded-full ${
+                  tradingAccStatus ? "animate-pulse bg-green-600" : "bg-red-500"
                 }`}
               />
               <p
-                className={`text-xs ${
+                className={`text-md ${
                   tradingAccStatus ? "text-teal-500" : "text-red-500"
                 }`}
               >
