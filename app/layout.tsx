4c15a01b479c84eb4f8df1e012d31d0fe690b04a
joshydavid <cxang.2022@scis.smu.edu.sg>
2026-03-28 13:06:55 +0800

    chore: update favicon
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,19 +21,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/favicon-light.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/favicon-dark.png",
         media: "(prefers-color-scheme: dark)",
       },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
     ],
-    apple: "/apple-icon.png",
+    apple: "/apple-touch-icon.png",
   },
 }
 
