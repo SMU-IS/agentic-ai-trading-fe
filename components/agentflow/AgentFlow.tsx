16807726454394ab5475f7f381620da1495b4a09
shawn <shawnkharace@gmail.com>
2026-04-03 01:59:23 +0800

    fix: agentflow zoom nodes landing page

--- components/agentflow/AgentFlow.tsx
+++ components/agentflow/AgentFlow.tsx
@@ -465,7 +465,8 @@ function AgentFlowContent({
     )
 
     const isMobile = window.innerWidth < 768
-    const zoom = isMobile ? 0.4 : 1
+    const isLanding = window.innerWidth < 1024
+    const zoom = isMobile ? 0.4 : isLanding ? 0.7 : 1
     const x = window.innerWidth / 2 - absoluteX * zoom - 110
     const y = (window.innerHeight * 0.7) / 2 - absoluteY * zoom - 150
     setViewport({ x, y, zoom }, { duration: 800 })
